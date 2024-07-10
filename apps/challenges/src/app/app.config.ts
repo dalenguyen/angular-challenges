bfb693b8fe7ec85f1e3acfdb71e600830967684f
Dale Nguyen <dale@dalenguyen.me>
2024-07-09 22:08:09 -0400

    chore: add angular material

--- apps/challenges/src/app/app.config.ts
+++ apps/challenges/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(appRoutes), provideAnimationsAsync()],
 };
