8ee18743c524245fe0df27093e22d4a0fc6d2dbf
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-07-15 03:35:07 +0000

    fix: remove unnecessary common module

--- apps/challenges/src/app/multilevel-data-passing/mldp-grand-children/mldp-grand-children.component.ts
+++ apps/challenges/src/app/multilevel-data-passing/mldp-grand-children/mldp-grand-children.component.ts
@@ -1,10 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'angular-challenges-mldp-grand-children',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   template: `
     <h4>Grand Child</h4>
     <p>Data from parent: {{ data() }}</p>
